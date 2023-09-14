52397ad78b906a1c1e7c6d2f167ca8b4dea27322
Arghyadeep Debnath <70471052+arghyadeep-debnath@users.noreply.github.com>
2023-09-14 23:08:14 +0530

    Update DHT_11_Test.ino

--- DHT_11_Test/DHT_11_Test.ino
+++ DHT_11_Test/DHT_11_Test.ino
@@ -1,3 +1,26 @@
+/*
+MIT License
+
+Copyright (c) 2023 Arghyadeep Debnath
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 #include <DHT.h>
 #include <DHT_U.h>
 
@@ -31,7 +54,7 @@ void loop() {
   
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("DHT_11 Interfacing problem; please check the connection!");
     return;
   }
 
